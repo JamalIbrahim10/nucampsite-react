2a3d776bf0595843d1dd8dc527e3f81230fe2612
Jamal Ibrahim <jamalibrahim@jamals-mbp.attlocal.net>
2020-07-11 12:05:05 -0700

    week-3 workshop

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponents';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { CAMPSITES } from './shared/campsites';
+
+
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
-    
-  render() {
+
+    render() {
       return (
+          <BrowserRouter>
           <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory campsites={this.state.campsites} />
+              <Main />
           </div>
+          </BrowserRouter>
       );
   }
 }
